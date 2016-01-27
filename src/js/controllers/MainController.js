e3d0edf97d0e4de71a4f35acf9b74a28f0604a91
Darien Lombardi <darien.lombardi@gmail.com>
2016-01-26 21:57:15 -0500

    uniform blurb responsive styling done

--- src/js/controllers/MainController.js
+++ src/js/controllers/MainController.js
@@ -9,27 +9,40 @@ app.controller('MainController', ['$scope', '$window', '$timeout', '$document', 
       bullets: [
         "Perform complete Inventory reconciliation without leaving your desk",
         "Deliver bank audits without the need for a physical count",
-        "Know when and where a car is an who has accessed the vehicle"
+        "Enhance your DMS system with actual location"
       ]
     },
     {
       title: "Vehicle Security",
       image: '../../assets/images/shield.png',
-      description: 'Know when a vehicle is accessed without authorization and locaiton known 24/7',
-      bullets: ["Without the key in the vehicle, you know the second a door is opened", "Customize the alert times and who gets them - including security", "All the other vehicles are aware of what is happening to the other around it"]
+      description: 'Know when a vehicle is accessed without authorization and know its location 24/7',
+      bullets: [
+        "Without the key in the vehicle",
+        "you know the second a door is opened",
+        "Customize the alert times and who gets them - including security",
+        "Surrounding vehicles are aware of what is happening to the other around it"
+      ]
     },
     {
       title: "Vehicle Positioning",
       image: '../../assets/images/map_marker.png',
       description: 'Position of the car on the lot',
-      bullets: ["Mobile app or web browser", "Know where the vehicle is on the lot", "With the presence of the mobile App, know the location of any vehicle off the lot"]
+      bullets: [
+        "Mobile app or web browser",
+        "Know where the vehicle is on the lot",
+        "With the presence of the mobile App, know the location of any vehicle off the lot"
+      ]
 
     },
     {
       title: "Vehicle Statistics",
       image: '../../assets/images/speedometer_96.png',
-      description: 'Vehicle communicates with you and logs all users and activity',
-      bullets: ["Where and who test drives the vehicle", "How much Gas and Battery is left", "How long it has been sitting in one place"]
+      description: 'Vehicle life-cycle history of events',
+      bullets: [
+        "Where and who test drives the vehicle",
+        "How much Gas and Battery is left",
+        "How long it has been sitting in one place"
+      ]
     }
   ];
 
@@ -53,7 +66,7 @@ app.controller('MainController', ['$scope', '$window', '$timeout', '$document', 
         "Secure wireless router in dealership",
         "Brain of the proprietary network",
         "Reports vehicle data to Cloud Backend",
-        "Range over 300 yards from nearest vehicle"
+        "One Gateway will generally cover each individual lot"
       ]
     },
     {
